6e1216b537d8a2b382cd63c83387377a8035a1ab
Brandon Bloom <brandon@brandonbloom.name>
2012-12-01 14:09:41 -0800

    Use keywords, not symbols in test cases

--- test/qplan/core_test.clj
+++ test/qplan/core_test.clj
@@ -3,28 +3,28 @@
         qplan.core))
 
 (deftest midpoint-test
-  (let [constraint #{{:inputs '#{a b}
-                      :outputs '#{c}
-                      :f (fn [{:syms [a b]}]
-                           {'c (- (* 2 b) a)})}
-                     {:inputs '#{b c}
-                      :outputs '#{a}
-                      :f (fn [{:syms [b c]}]
-                           {'a (- (* 2 b) c)})}
-                     {:inputs '#{a c}
-                      :outputs '#{b}
-                      :f (fn [{:syms [a c]}]
-                           {'b (/ ( + a c) 2.0)})}}]
+  (let [constraint #{{:inputs #{:a :b}
+                      :outputs #{:c}
+                      :f (fn [{:keys [a b]}]
+                           {:c (- (* 2 b) a)})}
+                     {:inputs #{:b :c}
+                      :outputs #{:a}
+                      :f (fn [{:keys [b c]}]
+                           {:a (- (* 2 b) c)})}
+                     {:inputs #{:a :c}
+                      :outputs #{:b}
+                      :f (fn [{:keys [a c]}]
+                           {:b (/ ( + a c) 2.0)})}}]
     (testing "Pre-solved"
-      (is (= '{a 10 b 20 c 30}
-             (evaluate [constraint] '[[a 10] [b 20] [c 30]]))))
+      (is (= {:a 10 :b 20 :c 30}
+             (evaluate [constraint] [[:a 10] [:b 20] [:c 30]]))))
     (testing "Moved endpoint"
-      (is (= '{a 15 b 20 c 25}
-             (evaluate [constraint] '[[a 15] [b 20] [c 30]])))
-      (is (= '{a 15 b 22.5 c 30}
-             (evaluate [constraint] '[[a 15] [c 30] [b 20]]))))
+      (is (= {:a 15 :b 20 :c 25}
+             (evaluate [constraint] [[:a 15] [:b 20] [:c 30]])))
+      (is (= {:a 15 :b 22.5 :c 30}
+             (evaluate [constraint] [[:a 15] [:c 30] [:b 20]]))))
     (testing "Moved midpoint"
-      (is (= '{a 10 b 25 c 40}
-             (evaluate [constraint] '[[b 25] [a 10] [c 30]])))
-      (is (= '{a 20  b 25 c 30}
-             (evaluate [constraint] '[[b 25] [c 30] [a 10]]))))))
+      (is (= {:a 10 :b 25 :c 40}
+             (evaluate [constraint] [[:b 25] [:a 10] [:c 30]])))
+      (is (= {:a 20  :b 25 :c 30}
+             (evaluate [constraint] [[:b 25] [:c 30] [:a 10]]))))))
