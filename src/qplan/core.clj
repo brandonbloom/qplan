297a279c65d13dd4b92dd4ee11e7b54794d8d98c
Brandon Bloom <brandon@brandonbloom.name>
2012-11-30 18:41:29 -0800

    Docstrings!

--- src/qplan/core.clj
+++ src/qplan/core.clj
@@ -1,62 +1,32 @@
 (ns qplan.core
+  "Provides a solver for multi-way, multi-output dataflow property models.
+  See Bradley Vander Zanden (1996) and Freeman, Jarvi, & Smith (2009).
+
+  The primary interface for this library is the evaluate function. See its
+  docstring for usage.
+
+  A dataflow problem can be represented by a hypergraph whose vertices are
+  variables and whose hyperedges are constraints. However, here, dataflow
+  problems are represented by a biparite graph whose vertices are variables
+  and methods. A method is a function from some set of variables to some
+  disjoint set of variables. Each constraint must specify some set of methods,
+  a subset of which can be evaluated to satisfy the constraint. The directed
+  edges of the biparite graph connect variables to the inputs of methods and
+  then from the outputs of methods back to variables. In this representation,
+  constraints need not be reified during solving. Instead, the notion of a
+  method's neighborhood is used to represent all methods produced by the same
+  constraint."
   (:require [clojure.set :as set]))
 
-(defmacro dbg [x]
-  `(let [x# ~x]
-     (println)
-     (println (pr-str '~x) "<<<")
-     (clojure.pprint/pprint x#)
-     (println ">>>\n")
-     x#))
 
-;;;
-
-;; constraint:
-;;   some set of variables
-;;   some set of methods for those variables
-;;   strength
-
-;; method:
-;; {:ins {}, :f f, :outs {}}
-;;
-;; a method is free if all of it's output variables are free
-;; a variable is free if it's only attached to one constraint
-
-;; Constraint graph
-; union of variables from all constraints
-; connection between all the variables via methods
-; constraints as hyper edges, so there is a place to look up strengths
-;     -- disj the methods ? or replace them with names of methods?
-;
-; G = <V + M, E>
-;   V -> variables
-;   M -> methods
-;   E -> directed edges connecting inputs to outputs through a method
-
-; when creating constraint graph, add stays
-
-; methods have input and output variables
-; variables have source and sink methods
-
-;; Needed operations
-; test if no more methods
-; get free methods from a constraint graph
-;    implies get methods for which a variable only has one constraint
-;    free methods should be sorted by ascending smallest number of output variables
-; remove a constraint and it's methods from a graph
-; add a method to a graph
-; get constraint with greatest strength
-; remove constraint from set of constraints
-
-
-; constraints sorted by strength descending    -- could call it "priority" and do ascending
-; variables sorted by number of source methods ascending
-; methods sorted by number of output variables ascending
-
-;; This is a naive algorithm, but I think methods is generally small here
-(defn free-method [methods neighborhoods]
+(defn- free-method
+  "Selects the best available free method from a given collection of methods
+  and a mapping from method to a neighborhood of methods."
+  [methods neighborhoods]
+  ;;TODO: Can this work be optimized out of the plan loop?
   (let [outputs (for [method methods, variable (:outputs method)]
                   [variable method])
+        ;; A variable is free if it can only be provided by one constraint.
         free-vars (->> outputs
                     (reduce (fn [sources [variable method]]
                               (let [source-set (conj (get sources #{}) method)]
@@ -64,12 +34,19 @@
                             {})
                     (filter #(= (-> % val count) 1))
                     keys set)
+        ;; A method is free, if all of its output variables are free.
         free-methods (filter (fn [{:keys [outputs] :as method}]
                                (every? free-vars outputs))
                              methods)]
+    ;; Prefer methods which output fewer variables.
     (apply min-key #(-> % :outputs count) free-methods)))
 
-(defn plan [methods neighborhoods]
+(defn- plan
+  "Returns a set of methods which can be traversed to to reassign each
+  variable at most once. Exactly one method will be selected from each
+  neighborhood represented in the methods set. Additional methods in the
+  neighborhoods mapping will be ignored. Returns nil if no solution exists."
+  [methods neighborhoods]
   (loop [satisfied #{}, unsatisfied methods]
     (if (empty? unsatisfied)
       satisfied
@@ -77,14 +54,21 @@
         (recur (conj satisfied method)
                (set/difference unsatisfied (neighborhoods method)))))))
 
-
-(defn make-neighborhoods [constraints]
+(defn- make-neighborhoods
+  "Given a collection of constraints, returns a mapping of each method to
+  the set of methods owned by the same constraint."
+  [constraints]
   (into {} (for [c constraints
                  :let [methods (:methods c)]
                  m methods]
              [m methods])))
 
-(defn solve [required prioritized]
+(defn- solve
+  "Given a collection of required constraints and a sequence of prioritized
+  constraints, returns a solution, as per the plan function, optimized to
+  maximize the number of satisfied prioritized constraints.
+  Returns nil if one or more required constraints can not satisfied."
+  [required prioritized]
   (let [neighborhoods (make-neighborhoods (concat required prioritized))
         methods (set (mapcat :methods required))]
     (when-let [solution (plan methods neighborhoods)]
@@ -100,30 +84,23 @@
               (recur solution* satisfied* unsatisfied*)
               (recur solution satisfied unsatisfied*))))))))
 
-(defn stay-constraint [variable value]
+(defn stay-constraint
+  "Returns a constraint which binds some variable to a constant value."
+  [variable value]
   {:methods #{{:inputs #{}
                :outputs #{variable}
                :f (constantly {variable value})}}})
 
-(defn evaluate [constraints values]
+(defn evaluate
+  "Given a collection of constraints and a sequence of [variable value] pairs
+  returns a map of variables to their new values. New values are selected such
+  that all constraints have been satisfied and minimizing the number of
+  changes subject to the prioritized order of the [variable value] pairs.
+  If the constraints cannot be satisfied, the result map preserves all values."
+  [constraints values]
   (let [stays (map #(apply stay-constraint %) values)
         variables (into {} values)]
     (reduce (fn [variables {:keys [inputs outputs f] :as method}]
               (into variables (f variables)))
             variables
             (solve constraints stays))))
-
-
-
-;; should sort methods by ascending number of constraints
-(def midpoint-constraint
-  {:variables '#{a b c}
-   :methods #{{:inputs '#{a b}
-               :outputs '#{c}
-               :f (fn [{:syms [a b]}] {'c (- (* 2 b) a)})}
-              {:inputs '#{b c}
-               :outputs '#{a}
-               :f (fn [{:syms [b c]}] {'a (- (* 2 b) c)})}
-              {:inputs '#{a c}
-               :outputs '#{b}
-               :f (fn [{:syms [a c]}] {'b (/ ( + a c) 2.0)})}}})
